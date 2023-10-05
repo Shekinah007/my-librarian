63621eb35d40f027bfc5810c21573572d93bdd39
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2023-10-05 17:57:47 +0100

    Changes to deploy to zeabur

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config'
+
 
 @Module({
   // imports: [BookModule, MongooseModule.forRoot('mongodb://0.0.0.0:27017/bookDatabase'),],
-  imports: [BookModule, MongooseModule.forRoot("mongodb+srv://shalomsheks:shekssheks@nodetuts.m5oxomk.mongodb.net/bookDatabase")],
+  imports: [ConfigModule.forRoot(), BookModule, MongooseModule.forRoot("mongodb+srv://shalomsheks:shekssheks@nodetuts.m5oxomk.mongodb.net/bookDatabase")],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
+
+
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
   app.setGlobalPrefix("library")
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
+// bootstrap();
+
+bootstrap().then(() =>
+  console.log(`Application is running on: ${process.env.PORT || 3000}`),
+);
+
+
