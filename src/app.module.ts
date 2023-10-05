d660f6b644c08d4b769649e4a63ed71b0a54cf08
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2023-10-05 17:40:46 +0100

    - Switch to database momgo atlas - Change nest start script in package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,8 @@ import { BookModule } from './book/book.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BookModule, MongooseModule.forRoot('mongodb://0.0.0.0:27017/bookDatabase'),],
-  // imports: [BookModule, MongooseModule.forRoot("mongodb+srv://shalomsheks:shekssheks@nodetuts.m5oxomk.mongodb.net/nestjs-demo")],
+  // imports: [BookModule, MongooseModule.forRoot('mongodb://0.0.0.0:27017/bookDatabase'),],
+  imports: [BookModule, MongooseModule.forRoot("mongodb+srv://shalomsheks:shekssheks@nodetuts.m5oxomk.mongodb.net/bookDatabase")],
   controllers: [AppController],
   providers: [AppService],
 })
